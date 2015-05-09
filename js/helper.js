3015681f0d34ec4ba791c304c346c382e92a035a
winhtaikaung <winhtaikaung28@hotmail.com>
2015-05-09 18:33:31 +0630

    new Branch Added

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 var helper={loadData:function(t){return $.ajax({url:t,success:function(t){},error:function(t){}})},DisableCache:function(){$.ajax({cache:!1})},ChartHelper:chartHelper},chartHelper={BarChartBuilder:function(t,e){$(t).highcharts({chart:{type:e.StringType},title:{text:e.StringTitle},subtitle:{text:e.StringSubtitle},xAxis:{categories:e.ArrayXcategories},yAxis:{min:0,title:{text:e.StringYtitle}},legend:{enabled:e.BooleanLegend},tooltip:{pointFormat:e.StringTooltip},series:e.ArraySeries})}};
-=======
-var helper={loadData:function(a){return $.ajax({url:a,success:function(a){},error:function(a){}})},DisableCache:function(){$.ajax({cache:!1})}};
->>>>>>> cc75e205c16b68afe73a41fbc440599eb67e0ca5
